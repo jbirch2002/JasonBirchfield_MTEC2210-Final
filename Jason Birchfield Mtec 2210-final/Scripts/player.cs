fbf07a6d91866f60b5104383952d715e454bf991
jbirch2002 <96215158+jbirch2002@users.noreply.github.com>
2024-04-18 17:30:57 -0400

    can move mouse now

--- Jason Birchfield Mtec 2210-final/Scripts/player.cs
+++ Jason Birchfield Mtec 2210-final/Scripts/player.cs
@@ -1,14 +1,27 @@
-using Godot;
+using Godot;	
 using System;
 
 public partial class player : CharacterBody3D
 {
-	public const float speed = 5.0f;
-	public const float jumpVelocity = 4.5f;
+	Node3D head = null;
+	float currentSpeed = 5.0f;
+	[Export]
+	float walkingSpeed = 5.0f;
+	[Export]
+	float runningSpeed = 8.0f;
+	float jumpVelocity = 4.5f;
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public const float mouseSens = 3f;
+
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
+	public override void _Ready()
+	{
+		head = GetNode<Node3D>("head");
+        Godot.Input.MouseMode = Godot.Input.MouseModeEnum.Captured;
+	}
+
+	#region PlayerMovement
 	public override void _PhysicsProcess(double delta)
 	{
 		ProcessPlayerInput(delta);
@@ -17,33 +30,73 @@ public partial class player : CharacterBody3D
 	public void ProcessPlayerInput(double delta)
 	{
 		Vector3 velocity = Velocity;
+        Vector2 inputDir = Godot.Input.GetVector("left", "right", "up", "down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 
 		if (!IsOnFloor())
 			velocity.Y -= gravity * (float)delta;
 
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+		if (Godot.Input.IsActionJustPressed("jump") && IsOnFloor())
 			velocity.Y = jumpVelocity;
-
-		Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 
 		if (direction != Vector3.Zero)
 		{
-			velocity.X = direction.X * speed;
-			velocity.Z = direction.Z * speed;
+			velocity.X = direction.X * currentSpeed;
+			velocity.Z = direction.Z * currentSpeed;
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, speed);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, currentSpeed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, currentSpeed);
 		}
 
-		if(Input.IsActionJustPressed("interact"))
+		if(Godot.Input.IsActionPressed("sprint") && IsOnFloor())
 		{
-			// 
+            currentSpeed = runningSpeed;
+		}
+		else
+		{
+            currentSpeed = walkingSpeed;
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
 	}
+	#endregion
+
+	#region Inputs
+	public override void _Input(InputEvent @event)
+	{
+		base._Input(@event);
+
+		if (@event is InputEventKey eventKey)
+		{
+			if (eventKey.Pressed && eventKey.Keycode == Key.Escape)
+			{
+				GetTree().Quit();
+			}
+			if(Godot.Input.IsActionJustPressed("interact"))
+			{
+			// interact logic here
+			}
+		}
+
+		if(Input.MouseMode == Godot.Input.MouseModeEnum.Captured)
+		{
+			InputEventMouseMotion mouseMotion = @event as InputEventMouseMotion;
+			if (mouseMotion != null)
+			{
+				HandleCameraRotation(mouseMotion);
+			}
+		}
+	}
+
+	void HandleCameraRotation(InputEventMouseMotion mouse)
+	{
+		head.RotateX(Mathf.DegToRad(-mouse.Relative.Y * mouseSens));
+		head.RotationDegrees = new Vector3(Mathf.Clamp(head.RotationDegrees.X, - 89, 89), head.RotationDegrees.Y, head.RotationDegrees.Z);
+		RotateY(Mathf.DegToRad(-mouse.Relative.X * mouseSens));
+	}
+
+	#endregion
 }
